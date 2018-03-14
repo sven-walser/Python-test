932aa65e847e14362da0d5b368d03d5ed4f8e9d2
Sven Walser <svenderlauch@gmx.ch>
2018-03-14 14:41:10 +0100

    mein eigener ordner

---
+++ selbstprojekte/namensbewerter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+print("Sag mir wie du heisst dann bewerte ich deinen Namen")
+
+name = input("Dein Name: ")
+
+name = name.lower()
+
+if name == "sven":
+	print("so ein schöner Name")
+elif name == "claudio":
+	print("Das ist der schlechteste Name den ich je gehört habe")
+elif name == "simon":
+	print("wow du warst also stärker als die Tür")
+elif name == "roman":
+	print("häufiger Name aber immer noch besser als CLaudio")
+else:
+	print("Dieser Name ist nicht so schön")
+
+
+
---
+++ selbstprojekte/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+geburtsmonat = input("Dein geburtsmonat ")
+
+if geburtsmonat == "März":
+	print("du bist aber jung")
+elif geburtsmonat == "juni":
+	print("du bist ganz normal")
+else:
+	print("alter sack!")
